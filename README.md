967dfdfa04ca3c1855af6f8c2047288058925124
Rehab <rehabmohd03@gmail.com>
2026-02-12 09:28:12 +0300

    done

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 #### [GitHub](https://github.com/rehabmohd03-beep/Memory-Game.git)
 
 ### ***Description***
-#### A brief description of the project The game contains 16 cards. All cards are turned face down, and you choose two. If the two cards have the same emoji , they disappear and you moves a point. Then you move on to the other cards until all the cards match and you win the game and have timer 55 second if time equal zero the board is Stop and try play game again because time finish.
+#### A brief description of the project the game contains 16 cards. all cards are turned face down, and then two cards are chosen. If the two cards have the same emoji, show green border , each time you choose two cards, the number of moves increases by one. You then move to the other cards if all the cards is  match, win the game and Start win music play, and you are taken to another page display the Win message, the number of seconds remaining, and the number of moves. You have a 60-second timer. If the timer finish and equal  zero, the game stop , display  loss message  with the loss music , and you try again play .
 ***
 
 ### ***Technologies Used***
@@ -25,7 +25,7 @@
 ##### Click on two emoji
 ##### Each time click two emoji the moves increase one
 ##### Have timer decrease start 60 seconds
-##### if finish open all cards you win and Display another page
+##### if finish open all cards you win and Display another page 
 ##### if time is finish and you don't finish all cards the board Stop and try play again
 
 
--- script.js
+++ script.js
@@ -1,93 +1,85 @@
-//  crate variable and select form class in memory html
-const playBoard = document.querySelector('.board') // a add dot because class name board
+//  crate variable and select form class in index  html
+const playBoard = document.querySelector('.board') // a add dot because class name is board
 const startButton = document.querySelector('button') // button from html
-const moves = document.querySelector('.moves') // a add dot because class name moves
-const time = document.querySelector('.timer') // a add dot because class name timer
+const moves = document.querySelector('.moves') // a add dot because class name is  moves
+const time = document.querySelector('.timer') // a add dot because class name is  timer
 // crate array to add emojis form
 //I copy emojis from this link https://emojipedia.org/
 const emojis = ['🍩', '🍰', '🍪', '☕', '🥐', '🍫', '🍟', '🍕'];
 
 // variable
-let openCard = null
-let lockBoard = false
+let openCard = null // add null because there is no open card at the beginnning
+let lockBoard = false // add false to can click on cards
 let moveCount = 0 // add zero to start from zero and increases
 let seconds = 60 // start number 60 and decrease
-let clock = null
+let clock = null // to store clock in clearInterval and can stop timer
 // variable musics
 let winMusic;
 let lossMusic;
-// https://www.w3schools.com/graphics/game_sound.asp link reference for music code
 
+// https://www.w3schools.com/graphics/game_sound.asp link reference for music code
+// create function name is start music
 function startMusic() {
      //https://pixabay.com/sound-effects/search/victory/?pagi=2 link for win music
-    winMusic = new sound("assets/winMusic.mp3");
+    winMusic = new Audio("assets/winMusic.mp3"); // name of music add
     //https://pixabay.com/sound-effects/search/lose/ link for web loss music
-    lossMusic = new sound("assets/lossMusic.mp3");
+    lossMusic = new Audio("assets/lossMusic.mp3"); //name for music add
 }
+
+
 //crate function name mix Cards with one parameter name arr each time random cards
-
-
+// sort
 function mixCards(arr) {
-  //add for loop start with length last one in array and the loop continue to zero each time -1
-  for (let i = arr.length - 1; i > 0; i--) {
-  // inside loop add random number  and two arr i and arr j and return arr all
-  // floor keep number int
-  // j is result location of index i chose
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]]
-  }
-  return arr;
+  return arr.sort(() => Math.random() - 0.5);
 }
-
 // crate function name build Board
 function buildBoard() {
-  // change board empty
-  playBoard.textContent = ''
+  playBoard.textContent = ''// change board empty  delete any card open befor
  // ... emojis add two can each emoji repeat
-  const cards = mixCards([...emojis,...emojis])
+  const cards = mixCards([...emojis,...emojis]) // mix emojis and duplication emojis
  // for loop start with zero each time increase one to length of cards
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < cards.length; i++) { // loop crate cards
     //add name card
-    const card = document.createElement('div')
-    card.className = 'card'
+    const card = document.createElement('div') // create element
+    card.className = 'card' // create class
     // add code html class name card-front and class name card-back
-    card.innerHTML =
+    card.innerHTML = // add card two face front and back
+    
   '<div class="card-front"></div>' +
   '<div class="card-back">' + cards[i] + '</div>'
   // when click on card is flip to show emoji
-  card.onclick = () => handleFlip(card)
+  card.onclick = () => handleFlip(card) // call function and compare the card if it matches
    // add card to play  in board
-    playBoard.append(card)
+    playBoard.append(card) // add card inside the board
   }
 }
 // note https://www.w3schools.com/jsref/met_win_settimeout.asp i copy time function from w3schools
 // add function name start time is measure in seconds
 
 function startTimer() {
-  clearTimeout(clock);
+  clearTimeout(clock); // stop any timer is before
 
   function sec() {
-    time.textContent = `Time: ${seconds} sec`;
+    time.textContent = `Time: ${seconds} sec`; // add text time and sec
 // if secound equals zero or less than
     if (seconds <= 0) {
-      // stop game
+      // stop play game
       lockBoard = true;
-      // start loss music play 
-     if(lossMusic) {
+      // start loss music play
           lossMusic.play();
-      }
-    const header2 = document.querySelector('h2');
-       header2.textContent = " The time is finish try play again";
-       header2.style.color = "red";
 
-      return;
+    const header2 = document.querySelector('h2'); // select h2 from index html
+       header2.textContent = " The time is finish try play again"; //add new message
+       header2.style.color = "red"; // add color red to message
+
+      return; // stop timer
     }
 
-    seconds--;
-    clock = setTimeout(sec, 1000);
+    seconds--; //decrease time by one
+    clock = setTimeout(sec, 1000); // restart function after 1 sec
   }
 
-  sec();
+  sec(); // call function to start time
 }
 
 // function to start play game
@@ -95,21 +87,22 @@ function startPlay() {
   moveCount = 0; // start moves zero
   seconds = 60 // start seconds 60
   openCard = null; // delete any open card
-  lockBoard = false;
-   startMusic();
-  playBoard.classList.remove('hidden'); // is remove board if second equel zero
+  lockBoard = false; // start game open
+   startMusic(); // call function
+  playBoard.classList.remove('hidden'); // is remove hidden to display board
   const header2 = document.querySelector('h2')
-  header2.textContent = "Start Play Memory Card Game";
-  header2.style.color = "white";
+  header2.textContent = "Start Play Memory Card Game"; // add message h2v
+  header2.style.color = "white"; //add color white to h2
       // display text moves and time
   moves.textContent = '0 moves';
   time.textContent = 'Time: 0 sec';
  // calls functions
-  buildBoard();
-  startTimer();
+  buildBoard(); // crate new cards
+  startTimer(); // start timer
 
 
 }
+// open card , save card , compares two cards , moves increase
  // crate handle flip with one parmeter name card
 const handleFlip = (card) => {
   // if board is lock or card is flipped
@@ -124,43 +117,53 @@ const handleFlip = (card) => {
 
   // start with second card
   moveCount++;
+  //save result
   moves.textContent = `${moveCount} moves`;
 
   checkCards(card); // check is two cards is Match emoji ?
 }
 //function to check cards
+// compare between open card and second card
 function checkCards(secondCard) {
   // is first card open is it equal second card
-  const isMatch = openCard.innerHTML === secondCard.innerHTML;
+  const isMatch =
+  openCard.querySelector('.card-back').textContent ===
+  secondCard.querySelector('.card-back').textContent;
    // if two card match open card is null and finish
   if (isMatch) {
     openCard.classList.add('matched');
     secondCard.classList.add('matched');
-    openCard = null;
-    checkFinish();
+    openCard = null; // remove open card
+    checkFinish(); // check if play finish
     //if is not match remove two card open and second
     //800 is time to flipped
   } else {
-    lockBoard = true;
+    lockBoard = true; // stop board
+    // 800 time is card open
     setTimeout(() => {
+      // card is closed
       openCard.classList.remove('flipped');
       secondCard.classList.remove('flipped');
-      openCard = null;
-      lockBoard = false;
+      openCard = null; // remove open card
+      lockBoard = false; //open game again
     }, 800);
   }
 }
 // function name check finish
+// fucation start music , stop time , save result , display win page
 function checkFinish() {
   //check how many card is flipped
+  //check card is open
   const matchedCards = document.querySelectorAll('.card.flipped').length;
   // add eight emojis in array  and * 2 to make total number of cards is 16
+  //check if the all card open and paly finish
+  //*2 becouse have 8 emoji and i need 16
   if (matchedCards === emojis.length * 2) {
      //  start music
-      if(winMusic)
-        { winMusic.play();}
 
-    clearInterval(clock);
+         winMusic.play();
+      // stop timer
+    clearTimeout(clock)
 
     // store result moves and time
 
@@ -170,21 +173,12 @@ function checkFinish() {
     setTimeout(() => {
       // go to other page name index.html
       //display result
-      window.location.href = "assets/win.html";
+      //after 6 sec go win page
+      window.location.href = "win.html";
     }, 6000);
   }
 }
 // when click on start button display board
 startButton.onclick = startPlay;
 
-function sound(src) {
-    this.sound = new Audio(src);
 
-    this.play = function() {
-        this.sound.play();
-    }
-
-    this.stop = function() {
-        this.sound.pause();
-    }
-}
